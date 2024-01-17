735cf02412b5056b0bcc4a4a157dd3202f516f05
Ted Pieplow <ted.pieplow@gmail.com>
2024-01-17 11:34:25 +0100

    changes made

--- Infrastructure/obj/Debug/net8.0/Infrastructure.AssemblyInfo.cs
+++ Infrastructure/obj/Debug/net8.0/Infrastructure.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Infrastructure")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b1d3fe5a06ae4af1fadad7251d55963743a6b443")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+12b24ac23ee7659f73dfd8a54a200008d91490a6")]
 [assembly: System.Reflection.AssemblyProductAttribute("Infrastructure")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Infrastructure")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
