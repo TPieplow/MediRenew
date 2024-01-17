f0799163d4e4a6ec50fd74c5be46b3883faed515
Kontiki94 <hassan.alheidari@hotmail.com>
2024-01-17 20:29:19 +0100

    testz

--- Infrastructure/obj/Debug/net8.0/Infrastructure.AssemblyInfo.cs
+++ Infrastructure/obj/Debug/net8.0/Infrastructure.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Infrastructure")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+516b023db08c34483d81ed69d5eaf6322d4d1cbe")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fdb4665816ba4a83a70458557f269d4522d81aa5")]
 [assembly: System.Reflection.AssemblyProductAttribute("Infrastructure")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Infrastructure")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
