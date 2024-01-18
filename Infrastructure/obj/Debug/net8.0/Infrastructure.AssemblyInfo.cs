93852a1467eec12d35de32533aac10b7070ecbca
Kontiki94 <hassan.alheidari@hotmail.com>
2024-01-18 17:03:31 +0100

    removed snapshot

--- Infrastructure/obj/Debug/net8.0/Infrastructure.AssemblyInfo.cs
+++ Infrastructure/obj/Debug/net8.0/Infrastructure.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Infrastructure")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a18d331c1986a96818e04719bf824a87b6fedc8e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9ab26081be2162e81e4e3230415ab101ffbdf916")]
 [assembly: System.Reflection.AssemblyProductAttribute("Infrastructure")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Infrastructure")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
