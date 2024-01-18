b4f90fe53c6fd042ebefe96e9b2b5d224151d6a2
Kontiki94 <hassan.alheidari@hotmail.com>
2024-01-18 13:44:43 +0100

    fixing stuff

--- Infrastructure/obj/Debug/net8.0/Infrastructure.AssemblyInfo.cs
+++ Infrastructure/obj/Debug/net8.0/Infrastructure.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Infrastructure")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a15022471be16e278001903b66d9ac22b99a49b9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+72259aefb5a4008bbdb857895ecdb56d39b939b6")]
 [assembly: System.Reflection.AssemblyProductAttribute("Infrastructure")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Infrastructure")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
