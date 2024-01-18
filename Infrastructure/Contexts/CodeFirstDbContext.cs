c33ae3b9255a0f55c3a3c71e4b860a332ff07e7b
Ted Pieplow <ted.pieplow@gmail.com>
2024-01-18 13:49:50 +0100

    No more conflicts

--- Infrastructure/Contexts/CodeFirstDbContext.cs
+++ Infrastructure/Contexts/CodeFirstDbContext.cs
@@ -4,7 +4,30 @@ using Microsoft.EntityFrameworkCore;
 
 namespace Infrastructure.Contexts;
 
-public class CodeFirstDbContext
+public class CodeFirstDbContext(DbContextOptions<CodeFirstDbContext> options) : DbContext(options)
 {
+    public virtual DbSet<HospitalEntity> Hospitals { get; set; }
+    public virtual DbSet<DepartmentEntity> Departments { get; set; }
+    public virtual DbSet<DoctorEntity> Doctors { get; set; }
+    public virtual DbSet<StaffEntity> Staff { get; set; }
+    public virtual DbSet<PatientEntity> Patients { get; set; }
+    public virtual DbSet<PharmacyEntity> Pharmacys { get; set; }
+    public virtual DbSet<AppointmentEntity> Appointments { get; set; }
+    public virtual DbSet<RoomEntity> Rooms { get; set; }
+    public virtual DbSet<PrescriptionEntity> Prescriptions { get; set; }
+    public virtual DbSet<InvoiceEntity> Invoices { get; set; }
 
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+
+        //modelBuilder.Entity<AppointmentEntity>().HasKey(
+        //    nameof(AppointmentEntity.PatientId), nameof(AppointmentEntity.DoctorId));
+
+       
+        DataSeederHospital.HospitalSeeder(modelBuilder);
+
+
+        //lägg till mer konfig om det behövs.
+    }
 }
