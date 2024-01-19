eb7dbe2bbf6af6842515cfda3ba76600653a19a5
Ted Pieplow <ted.pieplow@gmail.com>
2024-01-19 09:24:02 +0100

    Added a AddPatient-method in patientservice

--- Business/Services/PatientService.cs
+++ Business/Services/PatientService.cs
@@ -1,8 +1,57 @@
-﻿using Infrastructure.Repositories;
+﻿using Infrastructure.Contexts;
+using Infrastructure.HospitalEntities;
+using Infrastructure.Repositories;
+using Microsoft.EntityFrameworkCore.ChangeTracking.Internal;
+using System.Diagnostics;
 
 namespace Business.Services;
 
-public class PatientService
+public class PatientService(PatientRepository patientRepository, CodeFirstDbContext codeFirstDbContext)
 {
-    private readonly PatientRepository _patientRepository;
+    private readonly PatientRepository _patientRepository = patientRepository;
+    private readonly CodeFirstDbContext _codeFirstDbContext = codeFirstDbContext;
+
+
+    public async Task<bool> AddPatient(PatientEntity newPatient)
+    {
+        try
+        {
+            if (_patientRepository.Exists(x => x.Id == newPatient.Id || x.Email == newPatient.Email))
+            {
+                return false;
+            }
+
+            await _patientRepository.Create(newPatient);
+            return true;
+        }
+        catch (Exception ex) { Debug.WriteLine($"ERROR : {ex.Message}"); }
+       
+
+    }
+
+    public async Task<PatientEntity> ViewPatient(int patientId)
+    {
+        try
+        {
+            if (_patientRepository.Exists(x => x.Id == patientId))
+            {
+                var patient = await _patientRepository.GetOne(x => x.Id == patientId);
+                if (patient is not null) 
+                {
+                    return patient;
+                }
+                else
+                {
+                    return null!;
+                }
+            }
+            return null!;
+        }
+        catch (Exception ex)
+        {
+
+            Console.WriteLine($"ERROR: {ex.Message}");
+            return null!; ;
+        }
+    }
 }
--- Infrastructure/obj/Debug/net8.0/Infrastructure.AssemblyInfo.cs
+++ Infrastructure/obj/Debug/net8.0/Infrastructure.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Infrastructure")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+87538ef9c8c87f9861f30a6852fd2307f473802c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7474c1328f204ae4a8b1ba40dbebfd92cadc9ebe")]
 [assembly: System.Reflection.AssemblyProductAttribute("Infrastructure")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Infrastructure")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
