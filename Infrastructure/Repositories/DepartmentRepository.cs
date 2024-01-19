1c5306be54f1360dbf526e4c7691d14774af089c
Ted Pieplow <ted.pieplow@gmail.com>
2024-01-19 07:25:56 +0100

    Added GetOne to BaseRepo

---
+++ Infrastructure/Repositories/DepartmentRepository.cs
@@ -0,0 +1,39 @@
+﻿using Infrastructure.Contexts;
+using Infrastructure.HospitalEntities;
+using System.Linq.Expressions;
+
+namespace Infrastructure.Repositories;
+
+public class DepartmentRepository : Repository<DepartmentEntity>
+{
+    private readonly CodeFirstDbContext _context;
+    public DepartmentRepository(CodeFirstDbContext context) : base(context)
+    {
+        _context = context;
+    }
+
+    public override Task<DepartmentEntity> Create(DepartmentEntity entity)
+    {
+        return base.Create(entity);
+    }
+
+    public override Task<bool> Delete(Expression<Func<DepartmentEntity, bool>> predicate)
+    {
+        return base.Delete(predicate);
+    }
+
+    public override Task<IEnumerable<DepartmentEntity>> GetAll()
+    {
+        return base.GetAll();
+    }
+
+    public override DepartmentEntity GetOne(Expression<Func<DepartmentEntity, bool>> predicate)
+    {
+        return base.GetOne(predicate);
+    }
+
+    public override Task<DepartmentEntity> Update(DepartmentEntity entity)
+    {
+        return base.Update(entity);
+    }
+}
--- Infrastructure/obj/Debug/net8.0/Infrastructure.AssemblyInfo.cs
+++ Infrastructure/obj/Debug/net8.0/Infrastructure.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Infrastructure")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+44f7485964d9cdbdbe0c0ae1cb55cabefab02f6c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f5c717e612e0ed8f559f695c116bd7ab64071f57")]
 [assembly: System.Reflection.AssemblyProductAttribute("Infrastructure")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Infrastructure")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
