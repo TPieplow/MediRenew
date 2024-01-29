8a8b9705b6399d13a62a662e4fadf2e9c844b3d4
Kontiki94 <hassan.alheidari@hotmail.com>
2024-01-29 12:42:11 +0100

    pull

--- Infrastructure/Repositories/AppointmentRepository.cs
+++ Infrastructure/Repositories/AppointmentRepository.cs
@@ -1,18 +1,17 @@
 ﻿using Infrastructure.Contexts;
 using Infrastructure.HospitalEntities;
 using Microsoft.EntityFrameworkCore;
-using System;
-using System.Diagnostics;
-using System.Linq.Expressions;
-using static Infrastructure.Utils.ResultEnums;
+
 
 namespace Infrastructure.Repositories
 {
     public class AppointmentRepository(CodeFirstDbContext context) : BaseRepository<AppointmentEntity>(context)
     {
+        private readonly CodeFirstDbContext _context = context;
+
         public override async Task<IEnumerable<AppointmentEntity>> GetAllAsync()
         {
-            var appointments = await context.Appointments
+            var appointments = await _context.Appointments
                 .Include(x => x.Patient)
                 .Include(x => x.Doctor)
                 .ToListAsync();
--- Infrastructure/obj/Debug/net8.0/Infrastructure.AssemblyInfo.cs
+++ Infrastructure/obj/Debug/net8.0/Infrastructure.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Infrastructure")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7e6ce05342d98924f92eb66ccdab7d74eb9a9861")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+46a6821cae1b0837aa221fea49db1d9154fef91f")]
 [assembly: System.Reflection.AssemblyProductAttribute("Infrastructure")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Infrastructure")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
