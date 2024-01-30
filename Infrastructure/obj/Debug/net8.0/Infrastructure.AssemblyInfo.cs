12f6b28469cbc2a3dfed9105dc85e82313aba036
Kontiki94 <hassan.alheidari@hotmail.com>
2024-01-30 13:37:54 +0100

    merge

--- Infrastructure/obj/Debug/net8.0/Infrastructure.AssemblyInfo.cs
+++ Infrastructure/obj/Debug/net8.0/Infrastructure.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Infrastructure")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3a5f4db15ae4b4d97f3fd36405eb88bdd789e3cf")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a6ad174894a538761293778be1052ab405e53f18")]
 [assembly: System.Reflection.AssemblyProductAttribute("Infrastructure")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Infrastructure")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
