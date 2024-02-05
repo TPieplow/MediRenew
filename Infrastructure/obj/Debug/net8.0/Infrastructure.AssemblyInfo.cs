81c4978e6e9b755453db830f000fd093088565b0
Ted Pieplow <ted.pieplow@gmail.com>
2024-02-05 13:52:03 +0100

    merged with ted, removed designers from gitignore

--- Infrastructure/obj/Debug/net8.0/Infrastructure.AssemblyInfo.cs
+++ Infrastructure/obj/Debug/net8.0/Infrastructure.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Infrastructure")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d917c92d553b0eb80bb213127a5e6c95aefe5220")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1c4dd49f6f5e95c69074a1ee9cabcd79a40bfb45")]
 [assembly: System.Reflection.AssemblyProductAttribute("Infrastructure")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Infrastructure")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
